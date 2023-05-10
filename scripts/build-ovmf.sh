bba1798f249b9fc84b4fa093ac28806a2cd3d9d2
Roy Hopkins <rhopkins@suse.de>
2023-05-10 07:42:00 +0100

    Tidy edk2 build. Update readme.
    Signed-off-by: Roy Hopkins <rhopkins@suse.de>

--- scripts/build-ovmf.sh
+++ scripts/build-ovmf.sh
@@ -3,6 +3,13 @@ set -e
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+if [[ $1 == "debug" ]]; then
+    LOG_FILE="ovmf_debug.build"
+else
+    LOG_FILE="ovmf_release.build"
+fi
+rm $LOG_FILE
+
 pushd $SCRIPT_DIR/../edk2
 
 export PYTHON3_ENABLE=TRUE
@@ -11,17 +18,18 @@ export CC=/usr/bin/gcc-7
 
 # First build requires some initialisation
 if [ ! -d "$SCRIPT_DIR/../edk2/BaseTools/Source/C/bin" ]; then
-    git submodule update --init --recursive
-    make -C BaseTools -j $(nproc)
-    patch -p1 -i ../ovmf/svsm_edk2.patch
+    echo "Building OVMF base tools."
+    make -C BaseTools -j $(nproc) > $LOG_FILE 2> $LOG_FILE
 fi
 
 unset WORKSPACE
-source edksetup.sh --reconfig
+source edksetup.sh --reconfig >> $LOG_FILE 2>> $LOG_FILE
 
-if [ $1 == "debug" ]; then
-    build -a X64 -b DEBUG -t GCC5 -D DEBUG_ON_SERIAL_PORT -D DEBUG_VERBOSE -p OvmfPkg/OvmfPkgSvsmX64.dsc
+if [[ $1 == "debug" ]]; then
+    echo "Building OVMF(debug). Build output logged to edk2/$LOG_FILE."
+    build -a X64 -b DEBUG -t GCC5 -D DEBUG_ON_SERIAL_PORT -D DEBUG_VERBOSE -D SVSM_MODULE_FILE=../svsm.bin -p OvmfPkg/OvmfPkgSvsmX64.dsc >> $LOG_FILE 2>> $LOG_FILE
 else
-    build -a X64 -b RELEASE -t GCC5 -p OvmfPkg/OvmfPkgSvsmX64.dsc
+    echo "Building OVMF(release). Build output logged to edk2/$LOG_FILE."
+    build -a X64 -b RELEASE -t GCC5 -D SVSM_MODULE_FILE=../svsm.bin -p OvmfPkg/OvmfPkgSvsmX64.dsc >> $LOG_FILE 2>> $LOG_FILE
 fi
 popd
