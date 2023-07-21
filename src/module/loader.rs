90fedb069dc9b063cbe3bdb1ba368f784237f684
Roy Hopkins <rhopkins@suse.de>
2023-07-21 14:37:14 +0100

    svsm/task, svsm/module: Create tasks from memory-mapped ELF file
    Currently, for a task to be created from an ELF file the file must be
    entirely read into memory then passed to `create_elf_task()`. Now
    that the file system supports mapping of files to virtual memory we
    can update this function to take a file handle which prevents us from
    having to copy a file contents into memory in order to create a task
    from an ELF image.
    
    Signed-off-by: Roy Hopkins <rhopkins@suse.de>

--- src/module/loader.rs
+++ src/module/loader.rs
@@ -6,75 +6,51 @@
 
 extern crate alloc;
 
-use core::alloc::{GlobalAlloc, Layout};
-
 use alloc::string::String;
+use alloc::vec::Vec;
 
 use crate::error::SvsmError;
 use crate::fs::{list_dir, open, FileHandle};
-use crate::mm::alloc::ALLOCATOR;
-use crate::task::create_elf_task;
+use crate::task::{create_elf_task, TaskPointer};
 
-pub struct ModuleLoader {}
+pub struct ModuleLoader {
+    pub modules: Vec<Module>,
+}
 
 impl ModuleLoader {
     pub fn enumerate() -> Result<Self, SvsmError> {
+        let mut modules: Vec<Module> = Vec::new();
+
         let module_files = list_dir("/modules")?;
         for module in module_files {
             let path = String::from("/modules/") + &module.as_str();
             // Each module is an ELF file
             let module_file = open(path.as_str())?;
-            let module = Module::load(&module_file);
+            let module = Module::load(module_file);
             match module {
-                Ok(_) => log::info!("Module {} loaded ok", path),
+                Ok(m) => {
+                    modules.push(m);
+                    log::info!("Module {} loaded ok", path);
+                }
                 Err(_) => log::info!("Module {} load failed", path),
             }
         }
-
-        Ok(Self {})
+        Ok(Self { modules })
     }
 }
 
 pub struct Module {
-    layout: Layout,
-    mem: *mut u8,
-    size: usize,
+    _task: TaskPointer,
+    _file: FileHandle,
 }
 
 impl Module {
-    pub fn load(file: &FileHandle) -> Result<Self, SvsmError> {
-        let layout = Layout::from_size_align(file.size(), 4096)
-            .unwrap()
-            .pad_to_align();
-        let mem = unsafe { ALLOCATOR.alloc(layout) };
-        let buf = unsafe { core::slice::from_raw_parts_mut(mem, file.size()) };
-
-        let size = match file.read(buf) {
-            Ok(s) => s,
-            Err(_) => {
-                unsafe { ALLOCATOR.dealloc(mem, layout) };
-                return Err(SvsmError::Module);
-            }
+    pub fn load(file: FileHandle) -> Result<Self, SvsmError> {
+        let task = create_elf_task(&file, 0, None)?;
+        let module = Module {
+            _task: task,
+            _file: file,
         };
-        // Sizes must match or there is something wrong in the filesystem
-        assert_eq!(size, file.size());
-
-        let module = Module { layout, mem, size };
-        module.initialise()?;
-
         Ok(module)
-    }
-
-    fn initialise(&self) -> Result<(), SvsmError> {
-        let buf = unsafe { core::slice::from_raw_parts_mut(self.mem, self.size) };
-        let _task = create_elf_task(buf, 0, None);
-
-        Ok(())
-    }
-}
-
-impl Drop for Module {
-    fn drop(&mut self) {
-        unsafe { ALLOCATOR.dealloc(self.mem, self.layout) };
     }
 }
